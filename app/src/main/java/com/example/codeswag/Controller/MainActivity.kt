5647231bf3d35659777c79f5bfffaac481a17853
Frederico Gago <fredyisaac@gmail.com>
2020-04-04 09:49:07 +0100

    refracted for recycler view adapter

--- app/src/main/java/com/example/codeswag/Controller/MainActivity.kt
+++ app/src/main/java/com/example/codeswag/Controller/MainActivity.kt
@@ -4,21 +4,27 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ArrayAdapter
 import android.widget.Toast
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.codeswag.Adapters.CategoryAdapter
+import com.example.codeswag.Adapters.CategoryRecycleAdapter
 import com.example.codeswag.Model.Category
 import com.example.codeswag.R
 import com.example.codeswag.Services.DataService
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
-    lateinit var adapter : CategoryAdapter
+    lateinit var adapter : CategoryRecycleAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        adapter = CategoryAdapter(this, DataService.categories)
+        adapter = CategoryRecycleAdapter(this, DataService.categories)
         categoryListView.adapter = adapter
+
+        val layourManager = LinearLayoutManager(this)
+        categoryListView.layoutManager= layourManager
+        categoryListView.setHasFixedSize(true)
 
     }
 }
